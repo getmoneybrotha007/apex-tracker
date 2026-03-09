62afceb6fe9a9e6c77d8a3b7b57c1ed8e53e0981
getmoneybrotha007 <getmoneybrotha007@gmail.com>
2026-03-08 20:23:08 -0500

    server.py

--- server.py
+++ server.py
@@ -378,3 +378,7 @@ if __name__ == '__main__':
     init_db()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
+
+# This ensures DB is initialized when gunicorn imports the module
+with app.app_context():
+    init_db()
